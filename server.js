1a6cc2054fbcca5483aada7958f04245ca34b49c
Tali Saar <tali@rooster.co>
2017-08-05 17:43:34 -0700

    fixed order of files

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json({
   type: "application/vnd.api+json"
 }));
 
-require('./routing/apiRoutes.js')(app);
-require('./routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 
 
