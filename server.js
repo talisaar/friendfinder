aa7fbd5f4c6dbcea99df96518fb956ae9bc5acd7
Tali Saar <tali@rooster.co>
2017-08-05 17:38:42 -0700

    hmm

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var app = express();
+var PORT = process.env.PORT || 3002;
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+require('./routing/apiRoutes.js')(app);
+require('./routing/htmlRoutes.js')(app);
+
+
+
+app.listen(PORT, function() {
+
+	console.log("server is on");
+});
+
+
